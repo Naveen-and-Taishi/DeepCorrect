e40beb5057e3fced83324084cc880116ba1b7084
ns90001 <63938349+ns90001@users.noreply.github.com>
2021-12-07 15:01:55 -0500

    test

--- code/referee.py
+++ code/referee.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import numpy as np
 import random
 import math
+from tensorflow.keras.applications.vgg16 import VGG16
 
 class Referee(tf.keras.Model):
     def __init__(self) -> None:
