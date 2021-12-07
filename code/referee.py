a01681e9884902d1f9d880c036f4d044aa76ba55
Taishi Nishizawa <taishinishizawa@taishis-mbp.devices.brown.edu>
2021-12-07 14:59:33 -0500

    wrote requirements and defined referee arch

--- code/referee.py
+++ code/referee.py
@@ -3,21 +3,39 @@ import tensorflow as tf
 import numpy as np
 import random
 import math
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Reshape
+from tensorflow.math import exp, sqrt, square
+from tensorflow.keras.applications.vgg16 import VGG16
 
 class Referee(tf.keras.Model):
-    def __init__(self) -> None:
+    def __init__(self, batch_size) -> None:
         super(Referee, self).__init__()
 
         # TODO: Initialize all hyperparameters
-
+        self.learning_rate = 0.01
+        self.Adam = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
+        self.batch_size = batch_size
         # TODO: Initialize all trainable parameters
+        self.referee = Sequential([
+            VGG16(include_top=False, 
+            weights="imagenet"),
+            Dense((1,1,4096), activation="relu"),
+            Dense((1,1,4096), activation="relu"),
+            Dense((1,1,20), activation="relu")
+        ])
         pass
 
     def call(self, inputs):
         # TODO: Write forward-pass logic
-        pass
+        # outputs a 1 by 1 by 20 , normalized
+        refereeOutput = tf.nn.softmax(self.referee(inputs))
+
+        # this returns probability of input being one of the 20 images
+        return refereeOutput
     
-    def loss(self, probs, labels):
+    def loss(self, probs, labels): 
         # TODO: Write loss function
-        pass
+        loss = tf.keras.losses.sparse_categorical_crossentropy(labels, probs, from_logits=False)
+		return tf.reduce_sum(loss)
     
